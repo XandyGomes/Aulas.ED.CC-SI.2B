9b3443f2a5bd183f9c7d460c954a374217863ee4
XandyGomes <x3.computadores@hotmail.com>
2025-08-20 20:42:03 -0300

    Aula.20.08.2025

---
+++ data/bubble-sort-obj.mjs
@@ -0,0 +1,22 @@
+function bubbleSort(vetor, fnComp) {
+  let trocou;
+
+  do {
+    trocou = false;
+    for (let i = 0; i < vetor.length - 1; i++) {
+      if (fnComp(vetor[i] , vetor[i + 1])) {
+        [vetor[i], vetor[i + 1]] = [vetor[i + 1], vetor[i]];
+        trocou = true;
+      }
+    }
+  } while (trocou);
+}
+
+import { objMotoristas } from "./motoristas-obj-desord.mjs"
+
+console.time("Tempo de ordenação");
+bubbleSort(objMotoristas, (elem1, elem2) => 
+    elem1.nome_motorista > elem2.nome_motorista);
+console.timeEnd("Tempo de ordenação");
+
+console.log(objMotoristas)
